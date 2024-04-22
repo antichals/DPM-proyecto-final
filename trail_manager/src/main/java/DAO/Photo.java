05ed0ad1396248dbfacde93e05b49e07e6450107
Carlos Martinez <zefcma@hotmail.es>
2024-04-22 17:41:59 +0200

    Added path attribute and its methods to photo class

--- trail_manager/src/main/java/DAO/Photo.java
+++ trail_manager/src/main/java/DAO/Photo.java
@@ -3,8 +3,10 @@ package DAO;
 import java.util.Date;
 
 public class Photo {
+    // Attributes
     private int photoId;
     private int routeId;
+    private String projectPath;
     private String content;
     private int likesCount;
     private String location;
@@ -35,6 +37,14 @@ public class Photo {
 
     public void setRouteId(int routeId) {
         this.routeId = routeId;
+    }
+
+    public String getProjectPath() {
+        return projectPath;
+    }
+
+    public void setProjectPath(String projectPath) {
+        this.projectPath = projectPath;
     }
 
     public String getContent() {
